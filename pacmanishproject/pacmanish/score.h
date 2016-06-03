33d112315c38fd1a50cd8ec0540ce93f89414bbd
Rhys Richards <rhys.richards@hotmail.co.uk>
2016-06-03 22:30:20 +0100

    Score & Menu StartPoint
    Class diagrams added for both systems

---
+++ pacmanishproject/pacmanish/score.h
@@ -0,0 +1,11 @@
+//score.h
+
+class score
+{
+	int highscore[int];
+	float currentScore();
+	public:
+	void highscoreCalc(highscore[int]);
+	float currentScoreCalc();
+	void displayHighscore(highscore[int]);
+};
